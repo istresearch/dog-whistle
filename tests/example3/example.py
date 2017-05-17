5fb3926bae7316656a9db53a0b69644570165289
Marti Martinez <martinezah@gmail.com>
2017-05-17 09:56:47 -0700

    updating tests to reflect tighter  function call flagging

--- tests/example3/example.py
+++ tests/example3/example.py
@@ -5,3 +5,8 @@ logger.info("hey this is a message")
 logger.warn("stuff", extras)
 
 logger.info("bad" + str(val), extras)
+
+logger.info("good format stuff here")
+
+logger.warn("bad formatting here {}".format(url))
+
