15472425a515151b6d65907f02365c015627fff1
Marti Martinez <martinezah@gmail.com>
2017-05-18 13:26:03 -0700

    updating gauge regex to be non-greedy

--- tests/example4/example.py
+++ tests/example4/example.py
@@ -1,5 +1,5 @@
 logger = LogFactory.get_instance()
 
 logger.info('this is a test', extra={'foo': 'bar'})
-logger.warn('This is another "test"', extra={'foo': "bar"})
+logger.warn('This is another "test"', extra={'foo': "bar", 'bar': "baz", "lorem": "ipsum"})
 
