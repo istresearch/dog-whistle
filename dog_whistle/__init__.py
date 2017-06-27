1a1d703874fc2e6925205c4f37585d69cf3ac29f
Jeremy Steele <jeremy.steele@istresearch.com>
2017-06-27 15:47:06 -0400

    Allow tag override in extras

--- dog_whistle/__init__.py
+++ dog_whistle/__init__.py
@@ -284,7 +284,11 @@ def dw_callback(message, extras):
                     log.warning("Could not find key " + item['value'] + " inside extras")
                 else:
                     the_msg = _ddify(item['name'])
-                    _gauge(the_msg, value, tags=_dw_configuration['tags'])
+
+                    if 'tags' in extras:
+                        _gauge(the_msg, value, tags=extras['tags'])
+                    else:
+                        _gauge(the_msg, value, tags=_dw_configuration['tags'])
         # increment counter metric
         else:
             if message in _dw_configuration['metrics']['c_mapper']:
@@ -293,7 +297,10 @@ def dw_callback(message, extras):
             else:
                 the_msg = _ddify(message)
 
-            _increment(the_msg, tags=_dw_configuration['tags'])
+            if 'tags' in extras:
+                _increment(the_msg, tags=extras['tags'])
+            else:
+                _increment(the_msg, tags=_dw_configuration['tags'])
     else:
         log.warning("Tried to increment attribute before configuration")
 
